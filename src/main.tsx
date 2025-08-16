72b96d130be8c1e2a3ada029c879d210cc5fb7eb
arem4n <55893558+arem4n@users.noreply.github.com>
2025-08-15 22:44:43 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TommyBoxLanding from './TommyBoxLanding'
-
+import TommyBoxLanding from '../TommyBoxLanding';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <TommyBoxLanding />
