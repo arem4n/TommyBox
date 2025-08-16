ed97e09da1d4258dafb84ad3c00600946f0899b5
arem4n <55893558+arem4n@users.noreply.github.com>
2025-08-15 22:20:39 -0400

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TommyBoxLanding from './TommyBoxLanding'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <TommyBoxLanding />
+  </React.StrictMode>,
+)
